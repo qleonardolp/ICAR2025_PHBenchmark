0f9c2c45aafc231d90116a467bbaa670bdfa9d7c
Leonardo F. dos Santos <44267124+qleonardolp@users.noreply.github.com>
2025-08-18 03:23:59 -0300

    Add simulation bags, plot scripts and paper figures

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ICAR2025_PHBenchmark
+# Leveraging Port-Hamiltonian Theory for Impedance Control Benchmarking
+
+This repository host the data of the simulations scenarios present in the title paper. To reproduce the scenarios clone
+the following packages:
+
+- [ros2_descriptions](https://github.com/qleonardolp/ros2_descriptions)
+- [kinematic_pose_msgs](https://github.com/qleonardolp/kinematic_pose_msgs)
+- [impedance_reference_generator](https://github.com/qleonardolp/impedance_reference_generator)
+- [ros2_impedance_controller](https://github.com/qleonardolp/ros2_impedance_controller)
--- scripts/gait_analysis.py
+++ scripts/gait_analysis.py
@@ -15,8 +15,6 @@
 
 import matplotlib.pyplot as plt
 
-import numpy as np
-
 import pandas as pd
 
 from rclpy.serialization import deserialize_message
@@ -32,12 +30,13 @@ from rosidl_runtime_py.utilities import get_message
 
 import scienceplots  # noqa: F401
 
-bag_path = 'bags/step/'
+# from kinematic_pose_msgs.msg import KinematicPose
+
+bag_path = 'bags/gait/'
 
 topics = {
-    '/kinematic_reference/step_power': 'Float64',
-    '/impedance_controller/reference': 'KinematicPose',
-    '/impedance_controller/status': 'KinematicPose',
+    '/leg_impedance_controller/reference': 'KinematicPose',
+    '/leg_impedance_controller/status': 'KinematicPose',
 }
 
 reader = SequentialReader()
@@ -55,7 +54,7 @@ reader.set_filter(StorageFilter(topics=list(topics.keys())))
 
 # Collect
 t_begin = None
-series_names = {'reference', 'step_power', 'zpower'}
+series_names = {'foot_x', 'foot_z', 'cmd_power', 'ham_delta'}
 data = {name: [] for name in series_names}
 time = []
 
@@ -64,56 +63,48 @@ while reader.has_next():
     if topic_name not in types_filter:
         continue
 
-    msg = deserialize_message(raw, types_filter[topic_name])
-
-    if t_begin is None and topic_name != '/kinematic_reference/step_power':
-        continue
-
     if t_begin is None:
         t_begin = t_ns
 
     t = (t_ns - t_begin) / 1e9  # nanoseconds
     time.append(t)
 
-    if topic_name == '/kinematic_reference/step_power':
-        data['step_power'].append(msg.data)
+    msg = deserialize_message(raw, types_filter[topic_name])
 
-    if topic_name == '/impedance_controller/reference':
-        data['reference'].append(msg.pose.position.x)
-
-    if topic_name == '/impedance_controller/status':
-        data['zpower'].append(msg.pose_twist.angular.z)
-
+    if topic_name == '/leg_impedance_controller/status':
+        data['ham_delta'].append(msg.pose_twist.linear.x)
+        data['cmd_power'].append(msg.pose_twist.linear.y)
+        data['foot_x'].append(msg.pose.position.x)
+        data['foot_z'].append(msg.pose.position.z)
 
 df = pd.DataFrame({'t': time})
 for series in data:
     df[series] = pd.Series(data[series], index=range(len(data[series])))
 
-df = df[df['t'] <= 0.25]  # Slice
+df = df[(df['t'] > 1.4) & (df['t'] <= 4.0)]  # Slice
 
-# Moving average
-df['zpower_filt'] = df['zpower'].ewm(alpha=0.05, adjust=False).mean()
+# Integrate the command power
+df['cmd_energy'] = (df['cmd_power'] * df['t'].diff().fillna(0)).cumsum()
 
-rmse = np.sqrt(
-    np.mean(np.square(df['step_power'] - df['zpower_filt']))
-)
-
-print(f'Fidelity rms: {rmse}')
-# Result: 41.2148 W
+# Adjust the energy offset:
+df['ham_delta'] = df['ham_delta'] - df['ham_delta'].iloc[0]
 
 # Plot
 plt.style.use(['science', 'ieee'])
 
 fig, ax = plt.subplots()
 ax.set_xlabel('Time (s)')
-ax.set_ylabel('Power (W)')
-ax.plot(df['t'], df['zpower_filt'],
-        label=r'$[{y}^T{u}]_\Omega$',
+ax.set_ylabel('Energy (J)')
+ax.plot(df['t'], df['cmd_energy'],
+        label=r'$\int \dot{q}^T\tau_{act} dt$',
         linestyle='-', linewidth=0.8, color='blue')
-ax.plot(df['t'], df['step_power'],
-        label=r'${P}_{step}$', linestyle='-.', linewidth=0.8, color='k')
-ax.legend()
+ax.plot(df['t'], df['ham_delta'],
+        label=r'$({H}_q - {H}_\Omega)$',
+        linestyle='-.', linewidth=0.7, color='k')
+
+ax.set_ylim([-18.0, 6.0])
+ax.legend(loc='best', ncols=2)
 ax.grid(True, alpha=0.3)
 
-plt.savefig('figures/step_power_sim.png', dpi=300, bbox_inches='tight')
+plt.savefig('figures/gait_sim.png', dpi=300, bbox_inches='tight')
 plt.show()
