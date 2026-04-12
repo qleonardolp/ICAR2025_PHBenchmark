c624b00e54c5cae779ba33420f7cc0d084fa41ed
Leonardo F. dos Santos <44267124+qleonardolp@users.noreply.github.com>
2026-04-11 22:21:54 -0300

    Configure HyL impedance space visualization

--- tools/impedance_space.py
+++ tools/impedance_space.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from rclpy.serialization import deserialize_message
@@ -26,6 +27,8 @@ from rosidl_runtime_py.utilities import get_message
 import scienceplots  # noqa: F401
 from scipy.signal import butter, filtfilt
 
+# Cartesian axis index map
+axis_dict = {'x': 0, 'y': 1, 'z': 2, 'r': 3, 'p': 4, 'w': 5}
 
 def lpfilter(data, cutoff, sampling):
     """Zero phase low-pass filter to smooth timeseries."""
@@ -33,12 +36,18 @@ def lpfilter(data, cutoff, sampling):
     y = filtfilt(b, a, data, method='gust')
     return y
 
+# bag_path = '../../bags/rosbag2_2026_03_11-19_25_59/'
+bag_path = '../../sys_id/hyl_PRBS_K640D160/'
+controller_name = 'hyl_controller'
 
-bag_path = 'bags/rosbag2_2026_03_11-19_25_59/'
+# ATTENTION: set accordingly {x, y, z, r, p, w}
+e_idx = axis_dict['z'] + 6  # Jump the first 6 field
+de_idx = e_idx + 6
+dde_idx = de_idx + 6
 
 topics = {
-    '/ur5_controller/status': 'Float64MultiArray',
-    '/ur5_controller/reference': 'KinematicPose',
+    f'/{controller_name}/status': 'Float64MultiArray',
+    f'/{controller_name}/reference': 'KinematicPose',
 }
 
 reader = SequentialReader()
@@ -56,7 +65,7 @@ reader.set_filter(StorageFilter(topics=list(topics.keys())))
 
 # Collect
 t_begin = None
-series_names = {'e', 'de', 'dde'}
+series_names = {'e', 'de', 'dde', 'm'}
 data = {name: [] for name in series_names}
 time = []
 
@@ -66,7 +75,7 @@ while reader.has_next():
         continue
 
     # Align the timestamp based on status topic
-    if t_begin is None and topic_name != '/ur5_controller/status':
+    if t_begin is None and topic_name != f'/{controller_name}/status':
         continue
 
     if t_begin is None:
@@ -77,10 +86,13 @@ while reader.has_next():
 
     msg = deserialize_message(raw, types_filter[topic_name])
 
+    # filter Float64MultiArray messages
     if topic_name == list(topics.keys())[0]:
-        data['dde'].append(msg.data[18])
-        data['de'].append(msg.data[12])
-        data['e'].append(msg.data[6])
+        if np.isfinite(np.array(msg.data)).all():
+          data['dde'].append(msg.data[dde_idx])
+          data['de'].append(msg.data[de_idx])
+          data['e'].append(msg.data[e_idx])
+          data['m'].append(msg.data[4])  # m_zz
 
 df = pd.DataFrame({'t': time})
 for series in data:
@@ -88,25 +100,27 @@ for series in data:
     df.dropna(how='any', inplace=True)
 
 # Timeseries filtering
-fc = 25  # cutoff frequency [Hz]
-df['e_filt'] = lpfilter(df['e'], fc, 1000)
-df['de_filt'] = lpfilter(df['de'], fc, 1000)
-df['dde_filt'] = lpfilter(df['dde'], fc, 1000)
+filt = True
+if filt:
+    fc = 40.0  # cutoff frequency [Hz]
+    df['e_filt'] = lpfilter(df['e'], fc, 1000)
+    df['de_filt'] = lpfilter(df['de'], fc, 1000)
+    df['dde_filt'] = lpfilter(df['dde'], fc, 1000)
+else:
+    df['e_filt'] = df['e']
+    df['de_filt'] = df['de']
+    df['dde_filt'] = df['dde']
 
 # Time slice
-df = df[df['t'] <= 5.0]
+df = df[df['t'] > 1.0]
 
 # Plot
 # plt.style.use(['science', 'ieee'])
 
 fig, ax = plt.subplots()
 ax.set_xlabel('Time (s)')
-ax.plot(df['t'], df['e_filt'],
-        label=r'$\dot{e}$',
-        linestyle='-', linewidth=0.8, color='blue')
-ax.plot(df['t'], df['de_filt'],
-        label=r'$\ddot{e}$',
-        linewidth=0.8, color='k')
+ax.plot(df['t'], df['m'], label=r'$m_{zz}$', linestyle='-', linewidth=0.8, color='blue')
+# ax.plot(df['t'], df['de_filt'], label=r'$\ddot{e}$', linewidth=0.8, color='k')
 ax.grid(True)
 
 ax3 = plt.figure().add_subplot(projection='3d')
@@ -118,6 +132,6 @@ ax3.plot(df['e_filt'], df['de_filt'], df['dde_filt'],
          linestyle='-', linewidth=0.7, color='blue')
 
 ax3.legend(loc='upper left', columnspacing=0.5)
-ax3.grid(True, alpha=0.3)
+ax3.grid(True, alpha=0.25)
 
 plt.show()
